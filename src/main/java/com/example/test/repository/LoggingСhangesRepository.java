309af0ab26baf07d99ecc50538d9a58389664374
Buhancev <maxim.buhancev@gmail.com>
2023-07-15 03:18:04 +0700

    deleted useless imports

--- src/main/java/com/example/test/repository/LoggingСhangesRepository.java
+++ src/main/java/com/example/test/repository/LoggingСhangesRepository.java
@@ -4,8 +4,6 @@ import com.example.test.entity.LoggingСhanges;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface LoggingСhangesRepository extends JpaRepository<LoggingСhanges, Long> {
 
--- src/main/java/com/example/test/service/LoggingChangesService.java
+++ src/main/java/com/example/test/service/LoggingChangesService.java
@@ -6,8 +6,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
-
 @Service
 public class LoggingChangesService {
 
